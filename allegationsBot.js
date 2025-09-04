1434a548768bb013241f7a25dca1d73bbc89e238
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2025-09-04 04:55:23 -0400

    Update allegationsBot.js

--- allegationsBot.js
+++ allegationsBot.js
@@ -9,7 +9,9 @@ const ALLEGATIONS = [
   "Jinx was on the client list.",
   "No one knows where Goose World was on Jan 6th",
   "Zero Rangers opens clues on Entrana.",
-  "Tuna can't spell"
+  "Tuna can't spell",
+  "Tj's mom used to date Sean. She fucked around on him with some dirt bag and that guy dies a dozen years later. Sean still had resentment and I stood up for him. It causes a crazy argument and everybody turned on each other poor advice (Sean) hasn't played since. Tyrone Gilberg retaliated by taking the account that I spent thousands of hours on.",
+"Veryx isnt actually a vet."
 ];
 
 client.once("ready", () => {
